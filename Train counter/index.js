291e6d52dd03796f7da8153c25dd0835d1bc546b
Gitmeajob <114931219+Gitmeajob@users.noreply.github.com>
2022-11-12 14:41:21 -0600

    I am working on my starbucks duplicate website with random drink and food options.

--- Train counter/index.js
+++ Train counter/index.js
@@ -2,24 +2,36 @@
 
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-let count = 0
+let num = document.getElementById("incremented").value;
+let count = parseFloat(incremented);
+
 
  function increase() {
-     count += 1
-    countEl.textContent = count
+    if (count >= 250) {
+        alert('The train is full.')
+    } else {
+        countEl.textContent = count 
+        
+    }
  }
 
  let numLess = document.getElementById("count-el");
 
 function decrease() {
-    count -= 1
-    numLess.textContent = count
+    if(count <= 0) {
+        alert("You can't do that!");
+    } else {
+        numLess.textContent = -count;
+    }
  }
+
  function save() {
-     let countstr = count + " - "
+    if (count <= 0) {
+        alert('0 and negatives cannot board trains.');
+    } else {
+     let countstr = count + " - ";
      saveEl.textContent += countstr
      countEl.textContent = 0
      count = 0
-
-     console.log(count)
+    }
  }
